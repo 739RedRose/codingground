13c062464ba7c81cc167b9e67cedd65b408ab0ef
Red Rose <739redrose@users.noreply.github.com>
2017-01-21 15:53:36 -0400

    Create GameName.py

---
+++ GameName.py
@@ -0,0 +1,16 @@
+import random
+
+FirstName=['Soul','Crimson','Midnight']
+
+# Add more names
+ 
+SecondName=['Runner','Rose','Hour']
+ 
+# Add more names
+
+FN=random.choice(FirstName)
+SN=random.choice(SecondName)
+GamerName=FN+' '+SN
+ 
+print('Your Gammer Name Is')
+print(GamerName)
