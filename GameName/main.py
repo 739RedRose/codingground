6da79f0b8e00e2d488b9a9a1ae5bbba8dcdfd7d1
Red Rose <739redrose@users.noreply.github.com>
2017-01-21 16:06:34 -0400

    Created by Coding Ground Client

---
+++ GameName/main.py
@@ -0,0 +1,16 @@
+import random
+
+FirstName=['Crimson','Soul','Midnight']
+
+# Add more names to this
+
+SecondName=['Runner','Rose','Blade']
+
+# Add more names to this
+
+FN=random.choice(FirstName)
+SN=random.choice(SecondName)
+GameName=FN+' '+SN
+
+print("Your gammer/n's name is...")
+print(GameName)
