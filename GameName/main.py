ff86dd72e0f56aa1b9a12f1830a50adf4ae4133c
Red Rose <739redrose@users.noreply.github.com>
2017-05-10 20:21:15 -0400

    changed the format
    changed it to make it look better (;

--- GameName/main.py
+++ GameName/main.py
@@ -1,15 +1,15 @@
 import random
 
-FirstName=['Crimson','Soul','Midnight','Twilight']
+FirstName = ['Crimson', 'Soul', 'Midnight', 'Twilight']
 
 # Add more names to this
 
-SecondName=['Runner','Flower','Blade','Moon']
+SecondName = ['Runner', 'Flower', 'Blade', 'Moon']
 
 # Add more names to this
 
-FN=random.choice(FirstName)
-SN=random.choice(SecondName)
-GameName=FN+' '+SN
+FN = random.choice(FirstName)
+SN = random.choice(SecondName)
+GameName = FN + ' ' + SN
 
-print("Your gammer's name is: "+GameName)
+print("Your gammer's name is: " + GameName)
